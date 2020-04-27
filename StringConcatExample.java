dc7475f38481525fa6eeb0d08e6d7f8867726cf3
biiin <ravis_toy@naver.com>
2020-04-27 12:36:59 +0900

    add

---
+++ StringConcatExample.java
@@ -0,0 +1,13 @@
+
+public class StringConcatExample {
+    public static void main(String[] args) {
+        System.out.println("Hello, World"); 
+        String str1 = "Hello";
+        String str2 ="World";
+        String str3 = str1 + ", " + str2;
+        System.out.println(str3);  
+
+        System.out.println("result :  " + (3 + 30));
+        System.out.println((3 + 30) + "result :  ");
+    }
+}
