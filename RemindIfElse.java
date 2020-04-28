8b1c23fe99585a7f273da00fcd1a4029d4ad2a6b
cheabiiin <ravis_toy@naver.com>
2020-04-28 21:55:06 +0900

    HomeStudy

---
+++ RemindIfElse.java
@@ -0,0 +1,14 @@
+
+public class RemindIfElse {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if(score>=90) {
+            System.out.println("점수가 90보다 큽니다.");
+            System.out.println("등급은 A등급 입니다");
+        } else {
+            System.out.println("점수가 90보다 작습니다.");
+            System.out.println("등급은 B 입니다.");
+        }
+    }
+}
